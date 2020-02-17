4db7bdab194a71d1d28e83aff9e03004472707dd
Brend Lambert <brend.lambert@student.howest.be>
2020-02-17 11:42:09 +0800

    Temp search functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,29 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function get(Request $request) {
-        $size = is_numeric($request->input('size')) ? $request->input('size') : 6;
+        $size = is_numeric($request->input('size')) ? $request->input('size') : 25;
         $size = $size <= 100 ? $size : 100;
 
-        $res = User::orderBy('id', 'DESC')->paginate($size);
+        $searchTerms = $request->input('search');
+
+        $res = null;
+
+        if(is_null($searchTerms)) {
+            $res = User::orderBy('id', 'DESC')->paginate($size);
+
+        } else {
+            $searchTerms = explode(' ', $searchTerms);
+            $res = User::query();
+            foreach ($searchTerms as $term) {
+                $res->orWhere('first_name', 'LIKE', '%'.$term.'%');
+                $res->orWhere('middle_name', 'LIKE', '%'.$term.'%');
+                $res->orWhere('last_name', 'LIKE', '%'.$term.'%');
+                $res->orWhere('email', 'LIKE', '%'.$term.'%');
+            }
+
+            $res = $res->paginate($size);
+            //return response()->json(is_null($searchTerms) ? 'null' : $searchTerms);
+        }
 
         return UserResource::collection($res);
     }
