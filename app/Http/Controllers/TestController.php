fe317ad7184fc64915b1a6298d719ed33f6e06a1
Brend Lambert <brend.lambert@student.howest.be>
2020-02-17 08:20:22 +0800

    Comments explaining what each test is supposed to test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,14 +6,23 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+    /*
+     * Unauthenticated request test.
+     */
     public function get() {
         return response()->json(["data" => "TEST"], 200);
     }
 
+    /*
+     * low level authentication test.
+     */
     public function getAuth() {
         return response()->json(["data" => "TEST_AUTH"], 200);
     }
 
+    /*
+     * Admin permission type authentication test.
+     */
     public function getAdmin() {
         return response()->json(["data" => "TEST_ADMIN"], 200);
     }
