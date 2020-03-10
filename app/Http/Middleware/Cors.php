085e871b21f15a59d7136d2de5f0c90c24202a40
Brend Lambert <brend.lambert@student.howest.be>
2020-03-10 19:34:54 +0800

    custom Disable-Redirect header

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,7 +8,8 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
-            ->header('Access-Control-Expose-Headers', 'Authorization');
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, Disable-Redirect')
+            ->header('Access-Control-Expose-Headers', 'Authorization, Disable-Redirect')
+            ->header('Disable-Redirect', is_null($request->headers->get('Disable-Redirect')) ? 'false' : 'true');
     }
 }
