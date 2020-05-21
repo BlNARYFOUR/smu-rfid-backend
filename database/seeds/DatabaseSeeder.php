b8a9a226d73a0b16d6084db5ff136f86c2767e85
Brend Lambert <brend.lambert@student.howest.be>
2020-05-21 15:56:45 +0800

    Using tags haha finally useful

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,8 +27,8 @@ class DatabaseSeeder extends Seeder
         VehicleType::create(array('name' => 'motorcycle'));
         VehicleType::create(array('name' => 'bus'));
 
-        Vehicle::create(['vehicle_type_id' => 1, 'model' => 'Toyota', 'plate_number' => 'ABC123', 'or_number' => '123456', 'cr_number' => '7654321', 'licence_number' => '1234567', 'rfid_tag' => 'TEST901239TEST932109TEST', 'vehicle_owner_id' => 1, 'activated_at' => now(),]);
-        Vehicle::create(['vehicle_type_id' => 2, 'model' => 'Kawasaki Ninja H2R', 'plate_number' => 'XYZ890', 'or_number' => '654321', 'cr_number' => '1234567', 'licence_number' => '7654321', 'rfid_tag' => 'FOST321234FOST432123FOST', 'vehicle_owner_id' => 2, 'activated_at' => now()->subYears(1)->subMonth(),]);
+        Vehicle::create(['vehicle_type_id' => 1, 'model' => 'Toyota', 'plate_number' => 'ABC123', 'or_number' => '123456', 'cr_number' => '7654321', 'licence_number' => '1234567', 'rfid_tag' => 'E2003032761300940260ED01', 'vehicle_owner_id' => 1, 'activated_at' => now(),]);
+        Vehicle::create(['vehicle_type_id' => 2, 'model' => 'Kawasaki Ninja H2R', 'plate_number' => 'XYZ890', 'or_number' => '654321', 'cr_number' => '1234567', 'licence_number' => '7654321', 'rfid_tag' => 'E2001027680901430350E92F', 'vehicle_owner_id' => 2, 'activated_at' => now()->subYears(1)->subMonth(),]);
 
         User::create(['admin' => true, 'email' => 'admin@smu.test', 'first_name' => 'admin', 'last_name' => 'admin', 'password' => 'password', 'email_verified_at' => now(),]);
         User::create(['email' => 'user@smu.test', 'first_name' => 'user', 'last_name' => 'user', 'password' => 'password','email_verified_at' => now(),]);
